bf218675228536b012f8e4660aadb44f350ad3c4
Nico Weber <nicolasweber@gmx.de>
2013-06-19 08:57:15 -0700

    use lena image in morphology demo, show labels

--- ch01_morphology.py
+++ ch01_morphology.py
@@ -1,30 +1,39 @@
 from PIL import Image
 from pylab import *
 from scipy.ndimage import measurements, morphology
+from scipy import misc
 import numpy
 
-im = array(Image.open('board.jpeg').convert('L'))
+#im = array(Image.open('board.jpeg').convert('L'))
+im = misc.lena()
 im = 1 * (im < 128)
 gray()
 
-subplot(1, 3, 1)
+subplot(2, 3, 1)
 title('original')
 imshow(im)
 
-subplot(1, 3, 2)
+subplot(2, 3, 2)
 im_close = morphology.binary_closing(im, ones((5, 5)), iterations=2)
 title('closing')
 imshow(im_close)
 
-subplot(1, 3, 3)
+subplot(2, 3, 5)
+labels_close, obj_count_close = measurements.label(im_close)
+# FIXME: This seems to miss the left half of the face?
+imshow(labels_close)
+print obj_count_close, 'object on closing image'
+
+subplot(2, 3, 3)
 im_open = morphology.binary_opening(im, ones((5, 5)), iterations=2)
 title('opening')
 imshow(im_open)
 
-labels_close, obj_count_close = measurements.label(im_close)
+subplot(2, 3, 6)
 labels_open, obj_count_open = measurements.label(im_open)
-
-print obj_count_close, 'object on closing image'
+imshow(labels_open)
 print obj_count_open, 'object on opening image'
+
+#misc.imsave('out_labels.jpg', labels_open)
 
 show()
