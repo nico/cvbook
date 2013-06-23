3124069fbc3c811aaafbd0ddfad7482cac535ca9
Nico Weber <nicolasweber@gmx.de>
2013-06-23 16:33:42 -0700

    only compute sifts for new pano images

--- ch02_pano_sift.py
+++ ch02_pano_sift.py
@@ -3,6 +3,9 @@ import os
 import sift
 
 for f in glob.glob('out/*.jpg'):
-  print 'processing', f
   base = os.path.splitext(os.path.basename(f))[0]
-  sift.process_image(f, os.path.join('out', base + '.sift'))
+  sift = os.path.join('out', base + '.sift')
+  if os.path.exists(sift): continue
+
+  print 'processing', f
+  sift.process_image(f, sift)
