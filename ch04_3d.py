c09a675355208914334efd1d5c96d5ebb7dfb43c
Nico Weber <nicolasweber@gmx.de>
2013-07-20 15:49:29 -0700

    add set_modelview_from_camera

--- ch04_3d.py
+++ ch04_3d.py
@@ -23,6 +23,29 @@ def set_projection_from_camera(K):
   glViewport(0, 0, width, height)
 
 
+def set_modelview_from_camera(Rt):
+  glMatrixMode(GL_MODELVIEW)
+  glLoadIdentity()
+
+  # Rotate 90 deg around x, so that z is up.
+  Rx = numpy.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+  # Remove noise from rotation, make sure it's a pure rotation.
+  R = Rt[:, :3]
+  U, S, V = numpy.linalg.svd(R)
+  R = numpy.dot(U, V)
+  R[0, :] = -R[0, :]  # Change sign of x axis.
+
+  t = Rt[:, 3]
+
+  M = numpy.eye(4)
+  M[:3, :3] = numpy.dot(R, Rx)
+  M[:3, 3] = t
+
+  m = M.T.flatten()
+  glLoadMatrixf(m)
+
+
 def setup():
   pygame.init()
   pygame.display.set_mode((width, height), OPENGL | DOUBLEBUF)
@@ -32,6 +55,8 @@ def setup():
 setup()
 K = numpy.array([[1, 0], [0, 1]])  # FIXME
 set_projection_from_camera(K)
+Rt = numpy.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])  # FIXME
+set_modelview_from_camera(Rt)
 
 while True:
   event = pygame.event.poll()
