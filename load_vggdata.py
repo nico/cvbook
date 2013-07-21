3edc02ba16e012dd18af950de5fe4a6e0969a3bc
Nico Weber <nicolasweber@gmx.de>
2013-07-21 15:44:26 -0700

    add 2d visualizer of merton dataset

--- load_vggdata.py
+++ load_vggdata.py
@@ -8,7 +8,7 @@ im1 = numpy.array(Image.open('out_merton/images/001.jpg'))
 im2 = numpy.array(Image.open('out_merton/images/002.jpg'))
 
 points2D = [numpy.loadtxt(f).T for f in glob.glob('out_merton/2D/*.corners')]
-points2D = numpy.loadtxt('out_merton/3D/p3d').T
+points3D = numpy.loadtxt('out_merton/3D/p3d').T
 
 corr = numpy.genfromtxt('out_merton/2D/nview-corners', dtype='int', missing='*')
 
