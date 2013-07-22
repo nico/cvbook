7845982b24db74bade77c1c90ad8562777460dbc
Nico Weber <nicolasweber@gmx.de>
2013-07-21 17:20:47 -0700

    start at end-to-end 3d recovery example

---
+++ ch05_recover_3d.py
@@ -0,0 +1,45 @@
+from PIL import Image
+import glob
+import numpy
+import os
+import cPickle as pickle
+
+import camera
+import homography
+import sfm
+import sift
+import tic
+
+imname = glob.glob('out_corner/IMG_*.jpg')
+siftname = [os.path.splitext(im)[0] + '.sift' for im in imname]
+
+tic.k('start')
+
+l, d = {}, {}
+for i in range(len(imname)):
+  l[i], d[i] = sift.read_or_compute(imname[i], siftname[i])
+
+tic.k('loaded sifts')
+
+if not os.path.exists('out_ch05_recover_match.pickle'):
+  matches = sift.match(d[0], d[1])
+  pickle.dump(matches, open('out_ch05_recover_match.pickle', 'wb'))
+matches = pickle.load(open('out_ch05_recover_match.pickle', 'rb'))
+
+tic.k('matched')
+
+ndx = matches.nonzero()[0]
+x1 = homography.make_homog(l[0][ndx, :2].T)
+ndx2 = [int(matches[i]) for i in ndx]
+x2 = homography.make_homog(l[1][ndx2, :2].T)
+
+image = [numpy.array(Image.open(name)) for name in imname]
+
+# calibration (FIXME?)
+K = camera.my_calibration(image[0].shape[:2])
+
+# Normalize with inv(K) (allows metric reconstruction).
+x1n = numpy.dot(numpy.linalg.inv(K), x1)
+x2n = numpy.dot(numpy.linalg.inv(K), x2)
+
+# FIXME: estimate E, compute camera matrices, triangulate, plot
