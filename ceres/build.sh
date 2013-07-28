8d2f9e3be9b7aa9f980deb930cc81a6787d79f20
Nico Weber <nicolasweber@gmx.de>
2013-07-27 22:01:40 -0700

    build more stuff in build script

--- ceres/build.sh
+++ ceres/build.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
 THIS_DIR="$(dirname "${0}")"
-${THIS_DIR}/download-prerequisites.sh
+cd ${THIS_DIR}
+./download-prerequisites.sh
 
 mkdir -p install
 
-mkdir -p build
-cd build
-../gflags/configure --prefix=$PWD/../install
+mkdir -p build/eigen
+cd build/eigen
+cmake ../../eigen -DCMAKE_INSTALL_PREFIX=${PWD}/../../install
+make install
+cd ../..
+
+mkdir -p build/gflags
+cd build/gflags
+if ! [ -f config.status ]; then
+  ../../gflags/configure --prefix=${PWD}/../../install
+fi
 make -j10
 make install
+cd ../..
+
+mkdir -p build/glog
+cd build/glog
+if ! [ -f config.status ]; then
+  ../../glog/configure --with-gflags=${PWD}/../../install \
+                       --prefix=${PWD}/../../install
+fi
+make -j10
+make install
+cd ../..
+
+mkdir -p build/protobuf
+cd build/protobuf
+if ! [ -f config.status ]; then
+  ../../protobuf/configure --prefix=${PWD}/../../install
+fi
+make -j10
+make install
+cd ../..
