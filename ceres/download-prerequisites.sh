e58ab483532793645bce45e4146e2c97bea25069
Nico Weber <nicolasweber@gmx.de>
2013-07-27 21:20:03 -0700

    refactor

--- ceres/download-prerequisites.sh
+++ ceres/download-prerequisites.sh
@@ -2,23 +2,21 @@
 
 set -x
 
+fetch() {
+  dir=${1}
+  url=${2}
+  if ! [ -d ${dir} ]; then
+    curl -L -O ${url}
+    mkdir ${dir}
+    tar -xj -C ${dir} --strip-components 1 -f $(basename ${url})
+  fi
+}
+
 EIGEN_URL=http://bitbucket.org/eigen/eigen/get/3.2.0.tar.bz2
-if ! [ -d eigen ]; then
-  curl -L -O ${EIGEN_URL}
-  mkdir eigen
-  tar -xj -C eigen --strip-components 1 -f $(basename ${EIGEN_URL})
-fi
+fetch eigen $EIGEN_URL
 
 GLOG_URL=https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
-if ! [ -d glog ]; then
-  curl -L -O ${GLOG_URL}
-  mkdir glog
-  tar -xj -C glog --strip-components 1 -f $(basename ${GLOG_URL})
-fi
+fetch glog $GLOG_URL
 
 GFLAGS_URL=https://gflags.googlecode.com/files/gflags-2.0-no-svn-files.tar.gz
-if ! [ -d gflags ]; then
-  curl -L -O ${GFLAGS_URL}
-  mkdir gflags
-  tar -xj -C gflags --strip-components 1 -f $(basename ${GFLAGS_URL})
-fi
+fetch gflags $GFLAGS_URL
