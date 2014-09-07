f84ac43a65c3e8c3f79fc465dbea7f6c3bf3cc21
Nico Weber <nicolasweber@gmx.de>
2014-09-07 13:17:48 -0700

    ch08: let hand classifier demo print confusion matrix

--- ch08_classifyhands.py
+++ ch08_classifyhands.py
@@ -26,3 +26,19 @@ knn_classifier = knn.KnnClassifier(labels, features)
 res = numpy.array([knn_classifier.classify(feat, k) for feat in test_features])
 acc = numpy.sum(1.0 * (res == test_labels)) / len(test_labels)
 print 'Accuracy:', acc
+
+
+def print_confusion(res, labels, classnames):
+  n = len(classnames)
+  class_ind = dict([(classnames[i], i) for i in range(n)])
+
+  confuse = numpy.zeros((n, n))  # wow
+  for i in range(len(test_labels)):
+    confuse[class_ind[res[i]], class_ind[test_labels[i]]] += 1
+
+  print 'Confusion matrix for'
+  print classnames
+  print confuse
+
+
+print_confusion(res, test_labels, classnames)
